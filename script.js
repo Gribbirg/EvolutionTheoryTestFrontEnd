6a93d577a2c185dfc4abcacb4d34e03fb11b5985
Alexander Gribkov <gribkovalexander@gmail.com>
2023-11-28 11:07:40 +0300

    IOS fix try v1

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import data from './data.json' assert {type: 'json'};
+import data from 'https://gribbirg.github.io/EvolutionTheoryTestFrontEnd/data.json' assert {type: 'json'};
 
 let questions = [];
 let currentQuestionNum = 0;
