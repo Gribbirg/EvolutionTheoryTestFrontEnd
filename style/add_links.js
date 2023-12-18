dc60f04639578ecaf51bcaa915e25c7932f88f05
Alexander Gribkov <gribkovalexander@gmail.com>
2023-12-19 01:19:27 +0300

    Style add script

---
+++ style/add_links.js
@@ -0,0 +1,5 @@
+document.head.innerHTML += `
+    <link id="colors_link" rel="stylesheet" href="/centralized-testing-training/style/colors/tokens/orange-tokens.css">
+    <link id="theme_link" rel="stylesheet" href="/centralized-testing-training/style/colors/theme.css">
+    <link rel="stylesheet" href="/centralized-testing-training/style/global_style.css">
+`
