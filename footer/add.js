0320d23e93f55b8e0ba8f2718415e39dc0f13572
Alexander Gribkov <gribkovalexander@gmail.com>
2023-12-18 22:55:37 +0300

    Color choice add

--- footer/add.js
+++ footer/add.js
@@ -1,11 +1,24 @@
 document.body.innerHTML += `
-    <footer>
+<footer>
     <div>
-        Выберите цветовую тему:
+        Выберите тему:
         <div>
             <div><input type="radio" name="theme_input" id="theme+theme_input"><label for="theme+theme_input">Как на устройстве</label></div>
             <div><input type="radio" name="theme_input" id="theme.light+theme_input"><label for="theme.light+theme_input">Светлая тема</label></div>
             <div><input type="radio" name="theme_input" id="theme.dark+theme_input"><label for="theme.dark+theme_input">Темная тема</label></div>
+        </div>
+    </div>
+    <div>
+        Выберите цветовую тему:
+        <div>
+            <div><input type="radio" name="color_input" id="pink+color_input"><label for="pink+color_input"></label></div>
+            <div><input type="radio" name="color_input" id="red+color_input"><label for="red+color_input"></label></div>
+            <div><input type="radio" name="color_input" id="orange+color_input"><label for="orange+color_input"></label></div>
+            <div><input type="radio" name="color_input" id="yellow+color_input"><label for="yellow+color_input"></label></div>
+            <div><input type="radio" name="color_input" id="green+color_input"><label for="green+color_input"></label></div>
+            <div><input type="radio" name="color_input" id="blue+color_input"><label for="blue+color_input"></label></div>
+            <div><input type="radio" name="color_input" id="dark_blue+color_input"><label for="dark_blue+color_input"></label></div>
+            <div><input type="radio" name="color_input" id="purple+color_input"><label for="purple+color_input"></label></div>
         </div>
     </div>
     <address>
@@ -16,11 +29,14 @@ document.body.innerHTML += `
         </p>
         <p><small>Screaming Mushrooms 2023</small></p>
     </address>
-    </footer>
+</footer>
 `
 
 let theme = localStorage.getItem("theme") ?? "theme";
 setTheme(theme);
+
+let color = localStorage.getItem("color") ?? "orange";
+setColor(color);
 
 window.addEventListener("DOMContentLoaded", function () {
     document.getElementById(`${theme}+theme_input`).checked = true;
@@ -32,10 +48,25 @@ window.addEventListener("DOMContentLoaded", function () {
             }
         })
     });
+
+    document.getElementById(`${color}+color_input`).checked = true;
+    document.querySelectorAll(`input[name="color_input"]`).forEach(function (input) {
+        input.addEventListener("change", function () {
+            if (input.checked) {
+                color = input.id.split("+")[0];
+                setColor(color);
+            }
+        })
+    });
 });
 
 
 function setTheme(theme) {
-    document.getElementById("theme_link").href = `/centralized-testing-training/style/colors//${theme}.css`;
+    document.getElementById("theme_link").href = `/centralized-testing-training/style/colors/${theme}.css`;
     localStorage.setItem("theme", theme);
+}
+
+function setColor(color) {
+    document.getElementById("colors_link").href = `/centralized-testing-training/style/colors/tokens/${color}-tokens.css`;
+    localStorage.setItem("color", color);
 }
