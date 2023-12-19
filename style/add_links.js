75ae3880f2d8b4323ea1ff913682201b50c1a311
Alexander Gribkov <gribkovalexander@gmail.com>
2023-12-20 00:00:16 +0300

    Links fix

--- style/add_links.js
+++
@@ -1,5 +0,0 @@
-document.head.innerHTML += `
-    <link id="colors_link" rel="stylesheet" href="/centralized-testing-training/style/colors/tokens/orange-tokens.css">
-    <link id="theme_link" rel="stylesheet" href="/centralized-testing-training/style/colors/theme.css">
-    <link rel="stylesheet" href="/centralized-testing-training/style/global_style.css">
-`
