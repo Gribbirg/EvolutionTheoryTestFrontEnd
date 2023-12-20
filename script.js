f405a519e9a75c296e28dea62a6de62696cfecf2
Alexander Gribkov <gribkovalexander@gmail.com>
2023-12-20 03:04:18 +0300

    News add

---
+++ script.js
@@ -0,0 +1,20 @@
+"use strict"
+
+const response = await fetch("./data/news.json");
+const data = await response.json();
+
+initNews(data);
+
+function createNews(news) {
+    document.getElementById("news_div").innerHTML += `
+    <div class="news_element_div">
+        <h3>${news.name}</h3>
+        <p>${news.description}</p>
+        <small>${news["date"]}</small>
+    </div>
+    `
+}
+
+function initNews(data) {
+    data.forEach(createNews);
+}
